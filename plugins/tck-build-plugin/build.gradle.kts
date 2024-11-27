3c50e5b9659250fb048ddf19f0d048b67350cee6
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2024-11-27 11:28:27 +0100

    use separate test-plan generator plugin

--- plugins/tck-build-plugin/build.gradle.kts
+++ plugins/tck-build-plugin/build.gradle.kts
@@ -17,8 +17,15 @@ gradlePlugin {
             id = "org.eclipse.dataspacetck.tck-build"
             displayName = "TCK Build"
             description = "Gradle Plugin to customize the TCK build"
-            tags = listOf("tags", "dataspace", "dsp", "dcp", "tck", "plugins")
-            implementationClass = "org.eclipse.dataspacetck.plugins.tckbuild.TckBuildPlugin"
+            tags = listOf("tags", "dataspace", "dsp", "dcp", "tck", "plugins", "build")
+            implementationClass = "org.eclipse.dataspacetck.gradle.plugins.tckbuild.TckBuildPlugin"
+        }
+        create("tckGen") {
+            id = "org.eclipse.dataspacetck.tck-generator"
+            displayName = "TCK Test Plan Generator"
+            description = "Gradle Plugin to generate a test plan document in Markdown format"
+            tags = listOf("tags", "dataspace", "dsp", "dcp", "tck", "plugins", "test", "testplan", "markdown")
+            implementationClass = "org.eclipse.dataspacetck.gradle.plugins.tckgen.TckGeneratorPlugin"
         }
     }
 }
---
+++ plugins/tck-build-plugin/src/main/java/org/eclipse/dataspacetck/gradle/plugins/tckgen/TckGeneratorPlugin.java
@@ -0,0 +1,33 @@
+package org.eclipse.dataspacetck.gradle.plugins.tckgen;
+
+
+import org.eclipse.dataspacetck.gradle.tasks.GenerateTestPlanTask;
+import org.gradle.api.Project;
+import org.jetbrains.annotations.NotNull;
+
+import static java.util.Optional.ofNullable;
+
+/**
+ * Gradle plugin that adds a task to generate a test plan
+ */
+public class TckGeneratorPlugin implements org.gradle.api.Plugin<Project> {
+
+
+    @Override
+    public void apply(@NotNull Project target) {
+        System.out.println("YYEEEAHH " + target.getName());
+        // register the annotation processor task to all modules that declare a dependency onto the annotation processor,
+        // except the annotation processor module itself
+
+        ofNullable(target.getConfigurations().findByName("annotationProcessor"))
+                .ifPresent(c -> c.getDependencies().add(target.getDependencies().create("org.eclipse.dataspacetck:test-plan-generator:0.0.1")));
+
+        target.getTasks().register(GenerateTestPlanTask.NAME, GenerateTestPlanTask.class).configure(generateTestPlanTask -> {
+            generateTestPlanTask.setGroup("documentation");
+            // normally one would use getOrDefault, but the wildcard map prevents that
+            generateTestPlanTask.setImageFormat(ofNullable(target.getProperties().get("cvf.conversion.format")).map(Object::toString).orElse(("svg")));
+            generateTestPlanTask.setForceConversion(ofNullable(target.getProperties().get("cvf.conversion.force")).map(Object::toString).map(Boolean::parseBoolean).orElse(true));
+            // generateTestPlanTask.setOutputDirectory("/path/to/directory");
+        });
+    }
+}
